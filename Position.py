b6688367404184f4bcc3b8fdfe4e9cbb73f38ebe
jalbert90 <jalbert90@gmail.com>
2020-09-26 17:58:54 -0400

    Fixed typos

--- Position.py
+++ Position.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python3
 
 class Position:
-    positonCounter = 0
+    posCounter = 0
 
     def __init__(self, ticker, price, quantity):
         self.ticker = ticker
-        self.price = [price]
-        self.quantity = [quantity]
-        positionCounter += 1 
+        self.prices = [price]
+        self.quantities = [quantity]
+        Position.posCounter += 1 
 
-    def increasePosition(self, price, quantity):
-        self.price.append(price)
-        self.quantity.append(quantity)
+    def incPos(self, price, quantity):
+        self.prices.append(price)
+        self.quantities.append(quantity)
 
-    def decreasePositon(self, quantity):
+    def decPos(self, quantity):
         sellCounter = 0
         while sellCounter < quantity:
-            if self.quantity[0] > 1:
-                self.price[0] -= self.price[0] / self.quantity[0]
-                self.quantity[0] -= 1
+            if self.quantities[0] > 1:
+                self.prices[0] -= self.prices[0] / self.quantities[0]
+                self.quantities[0] -= 1
                 sellCounter += 1
             else:
-                self.price.pop(0)
-                self.quantity.pop(0)
+                self.prices.pop(0)
+                self.quantities.pop(0)
                 sellCounter += 1
 
-    def getPositonPrinciple(self):
+    def getPosPrinciple(self):
         sum = 0
-        for i in price:
+        for i in self.prices:
             sum += i
         return sum
 
-    def getPositionQuantity(self):
+    def getPosQuantity(self):
         quan = 0
-        for i in quantity:
+        for i in self.quantities:
             quan += i
         return quan
 
-    def getAveragePrice(self):
-        return self.getPositionPrinciple() / self.getPositionQuantity
+    def getAvgPrice(self):
+        return self.getPosPrinciple() / self.getPosQuantity
+
+shortPos = Position('SQQQ', 28.40, 1)
+shortPos.incPos(27.85, 1)
+shortPos.incPos(25.74, 1)
