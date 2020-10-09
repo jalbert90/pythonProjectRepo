ba859fde1d592387a612323acd10763ce4258066
jalbert90 <jalbert90@gmail.com>
2020-10-09 00:15:39 -0400

    Not Empty

--- csvPrac.py
+++ csvPrac.py
@@ -2,8 +2,15 @@
 
 import csv
 
-with open('2020-10-05-AccountStatement.csv', 'r') as fileHandle:
+with open('/home/jacob/positions.csv', 'r') as fileHandle:
     csv_reader = csv.reader(fileHandle)
 
-    for line in csv_reader:
-        print(line)
+    next(csv_reader)
+    next(csv_reader) 
+    next(csv_reader)
+
+    with open('positionTransposition.csv', 'w') as fileHandle2:
+        csv_writer = csv.writer(fileHandle2)
+
+        for row in csv_reader:
+            csv_writer.writerow(row)
